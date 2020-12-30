012ece7ef212393c920c302806f5a9e06db582d5
j-bonilla <Johnbonilla007@gmail.com>
2020-12-30 14:34:45 -0600

    add testing

--- kata-pair-programming-aklen.Tests/UnitTest1.cs
+++ kata-pair-programming-aklen.Tests/UnitTest1.cs
@@ -7,18 +7,25 @@ namespace kata_pair_programming_aklen.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void When_Sum_Input_Are_5_And_5_Return_Ten()
         {
+            CalculatorProgram calculator = new CalculatorProgram();
+            decimal expected = 10;
 
+            decimal result = calculator.SumNumbers(5, 5);
+
+            Assert.Equal(expected, result);
         }
 
         [Fact]
-        public void When_Sum_Input_Empty_Return_Zero()
+        public void When_Sum_Input_Are_Zero_Return_Zero()
         {
             CalculatorProgram calculator = new CalculatorProgram();
+            decimal expected = 1;
+
             decimal result = calculator.SumNumbers(0, 0);
 
-            Assert.Matches("result", "0");
+            Assert.Equal(expected, result);
         }
     }
 }
